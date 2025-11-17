20b3ab01cfa778e3e7e10a5b84041511484f44e7
Jason Kenney <jasonk@ameelio.org>
2025-11-17 13:15:54 -0500

    SC-10374 Initial implementation of a JWK serialization and deserialization.
    - This does not attempt to verify or encrypt/decrypt any field, it
      only leverages base64 to deserialize into binary.
    - This tries to mostly use base rust types to make using in different
      projects viable. It does have the tradeoff that downstream will have
      to implement conversions.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Json Web Key
+A ser(de) for jwks.
+
+## Contributing
+This crate at the moment is public, but we don't currently maintain it beyond the needs for our
+software.
--- src/rsa.rs
+++ src/rsa.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::hash::Hash;
 
 use crate::{cert::Certificate, encoded_bytes_field::EncodedBytesField};
 
@@ -24,6 +25,37 @@ pub struct RsaWebKey {
 #[serde(tag = "kty")]
 pub enum KeyType {
     RSA,
+}
+
+impl Default for RsaWebKey {
+    fn default() -> Self {
+        Self {
+            cert: Certificate::default(),
+            key_id: "".into(),
+            key_ops: [].into(),
+            key_type: KeyType::default(),
+            exponent: [].into(),
+            modulus: [].into(),
+            use_case: "".into(),
+        }
+    }
+}
+
+// Derive equality based off key_id, not accurate but --
+// just like database records, we can save a lot of
+// performance by just assuming the key_id is accurate.
+impl Eq for RsaWebKey {}
+
+impl Hash for RsaWebKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.key_id.hash(state)
+    }
+}
+
+impl PartialEq for RsaWebKey {
+    fn eq(&self, other: &Self) -> bool {
+        self.key_id.eq(&other.key_id)
+    }
 }
 
 impl Default for KeyType {
